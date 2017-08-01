f120aeea9421cbb81c7840c7000c38eb381c67b9
shoudong.qi <shoudong.qi@17zuoye.com>
2017-08-01 16:59:45 +0800

    工厂模式

---
+++ app/src/main/java/com/rollingalgo/design/creationModel/BluesFactoryPattern/Simple/BluesMZFactory.java
@@ -0,0 +1,8 @@
+package com.rollingalgo.design.creationModel.BluesFactoryPattern.Simple;
+
+/**
+ * Created by qishoudong on 2017/8/1.
+ */
+
+public class BluesMZFactory {
+}
---
+++ app/src/main/java/com/rollingalgo/design/structureModel/BluesCompositePattern/BluesCompositePattern.java
@@ -0,0 +1,38 @@
+package com.rollingalgo.design.structureModel.BluesCompositePattern;
+
+
+import com.rollingalgo.design.structureModel.BluesProxyPattern.BluesProxyTargetObject;
+
+/**
+ * Created by qishoudong on 2017/6/19.
+ * <p>
+ * currentType：组合模式,组合模式有时候又叫做部分-整体模式，
+ * 将对象组合成树形结构以表示“部分-整体”的层次结构。Composite使用户对单个对象和组合对象的使用具有一致性。
+ * <p>
+ * type：结构型  经常使用，
+ * <p>
+ * <p>
+ * <p>
+ * 何时使用：
+ *
+ *  当想表示对象的部分-整体层次结构。希望用户用一致的方式处理个体对象和组合对象
+ * <p>
+ * 优点：
+
+ * <p>
+ * <p>
+ * 缺点：
+ * <p>
+ * <p>
+ */
+
+
+public class BluesCompositePattern {
+
+
+    public static void main(String args[]) {
+        BluesProxyTargetObject object = new BluesProxyTargetObject();
+        object.method2();
+    }
+
+}
